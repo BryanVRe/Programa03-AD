fff38ceb9dc45a73e8c92dfd78d7098a6511bb5b
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2023-04-23 19:19:55 -0600

    programa03

---
+++ src/main/java/org/uv/programa03/ClaseAHija2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.programa03;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author carte
+ */
+
+public class ClaseAHija2 extends ClaseA{
+    
+    @Override
+    public void msg(){
+        Logger.getLogger(ClaseAHija2.class.getName()).log(Level.INFO, "Hola desde ClaseAHija2");
+    }
+}
---
+++ src/main/java/org/uv/programa03/ClaseConcreta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.programa03;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author carte
+ */
+public class ClaseConcreta {
+    public void imprimir(){
+    Logger.getLogger(ClaseConcreta.class.getName()).log(Level.INFO, "Hola desde clase concreta");
+    }
+}
